21cae09d3c1597aae3b92b40423762c890508631
lior <lior610.aharon@gmail.com>
2025-06-24 13:48:24 +0300

    removed unnecessary things from the app

--- app/main.py
+++ app/main.py
@@ -1,16 +1,6 @@
-from flask import Flask, request, jsonify
-import requests
-
-PUBLIC_IP_URL = 'https://api.ipify.org?format=json'
+from flask import Flask, request
 
 app = Flask(__name__)
-
-def get_ip_address():
-    res = requests.get(PUBLIC_IP_URL)
-    if res.status_code == 200:
-        return res.json().get('ip')
-    else:
-        return 'Unable to fetch IP address'
     
 def get_client_ip_address():
     x_forwarded_for = request.headers.get('X-Forwarded-For')
@@ -23,13 +13,9 @@ def get_client_ip_address():
         print(f"Client IP from remote_addr: {ip}")
     return ip
 
-def get_headers():
-    return dict(request.headers)
-
 @app.route('/')
 def index():
     return f'<h1>Your public IP address is: {get_client_ip_address()}</h1>'
-    # return jsonify(get_headers())
 
 
 if __name__ == '__main__':
